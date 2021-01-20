e51454b3d166c25a0ebd984c259b1153a90848c3
Egor Gorskikh <egorgorskikh@gmail.com>
2021-01-20 11:43:17 +0300

    Initial Commit

---
+++ Zaim-Express/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Zaim-Express
+//
+//  Created by Егор Горских on 20.01.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Zaim-Express/Zaim_ExpressApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zaim_ExpressApp.swift
+//  Zaim-Express
+//
+//  Created by Егор Горских on 20.01.2021.
+//
+
+import SwiftUI
+
+@main
+struct Zaim_ExpressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
